b2748dd2614f7740ef1ae3f1a459c515e451fd98
Ismael Bernal <ismaelbernal83@gmail.com>
2022-07-10 11:17:44 +0200

    Three more tests in Block.js, to check the nonce minning

--- src/core/Block.test.js
+++ src/core/Block.test.js
@@ -32,4 +32,25 @@ describe("Block",()=>{
         expect(block.previousHash).toEqual(lastBlock.hash);
     });
 
+    /**
+     * Hashes are generated alligned with difficulty
+     */
+    it('generates a hash that matches the difficutly',()=>{
+        expect(block.hash.substring(0,block.difficulty)).toEqual('0'.repeat(block.difficulty));
+    });
+
+    /**
+     * Lower the difficulty for a slower generated block
+     */
+    it('lower the difficulty for a slower generated block',()=>{
+        expect(Block.adjustDifficulty(block,block.timeStamp + 300000)).toEqual(block.difficulty - 1);
+    });
+
+    /**
+     * Raise difficulty for a faster generated block
+     */
+    it('raise the difficulty for a faster generated block',()=>{
+        expect(Block.adjustDifficulty(block, block.timeStamp + 1)).toEqual(block.difficulty + 1);
+    });
+
 })
